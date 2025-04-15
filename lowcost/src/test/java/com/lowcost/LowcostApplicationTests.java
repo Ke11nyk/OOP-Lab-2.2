be4508d5171417e19079b9d7eab1b1b5594a87a4
Artem Verbytskyi <116598786+Ke11nyk@users.noreply.github.com>
2025-04-15 22:08:27 +0300

    initial commit

---
+++ lowcost/src/test/java/com/lowcost/LowcostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowcost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowcostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
