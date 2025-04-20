623382f874f5da5540f3fc3bda4e6bc889ac82a0
Artem Verbytskyi <116598786+Ke11nyk@users.noreply.github.com>
2025-04-20 12:34:57 +0300

    caps roles

--- lowcost/src/main/java/com/lowcost/service/RoleUtil.java
+++ lowcost/src/main/java/com/lowcost/service/RoleUtil.java
@@ -3,15 +3,16 @@ package com.lowcost.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class RoleUtil {
-    public static final String ADMIN = "admin";
-    public static final String DISPATCH = "dispatch";
-    public static final String USER = "user";
+    public static final String ADMIN = "ADMIN";
+    public static final String DISPATCH = "DISPATCH";
+    public static final String USER = "USER";
 
     public static List<String> getAllowedRoles(String[] roles){
         return Arrays.asList(roles);
@@ -21,12 +22,20 @@ public class RoleUtil {
         return allowedRoles.contains(role);
     }
 
-    public static String getRole(String token){
-        Algorithm algorithm = Algorithm.HMAC256("baeldung");
-        JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer("Baeldung")
-                .build();
-        DecodedJWT decodedJWT = verifier.verify(token);
-        return decodedJWT.getClaim("role").asString();
+    public static String getRole(String token) {
+        if (token == null || token.isEmpty()) {
+            throw new IllegalArgumentException("Token cannot be null or empty");
+        }
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("baeldung");
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("Baeldung")
+                    .build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT.getClaim("role").asString();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Invalid token: " + e.getMessage());
+        }
     }
 }
